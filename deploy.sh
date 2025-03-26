0d8306004a82c6bb6f85396413f9930da56980a5
Subiksha P R <124233005+Subiksha1705@users.noreply.github.com>
2025-03-26 11:11:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ chmod +x build.sh
 
 # Docker Login
 echo "s1705sha17" | docker login -u subiksha17 --password-stdin
-docker tag learning_web subiksha17/myreact
-docker push subiksha17/myreact
+docker tag studcalc subiksha17/studentcalculator
+docker push subiksha17/studentcalculator
 
 
 # Deploy to Kubernetes
-kubectl create deployment myreact-app --image=subiksha17/myreact --port 80
-kubectl expose deployment myreact-app --type=NodePort --port=80
+kubectl create deployment calc --image=subiksha17/studentcalculator --port 80
+kubectl expose deployment calc --type=NodePort --port=80
 
 echo "Deployment successful! Access your app via Minikube service."
